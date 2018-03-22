1b2ce7389dcdb0b9ea438bd010dd608d85fab507
Eric Brehault <ebrehault@gmail.com>
2018-03-22 15:54:34 +0100

    fix public_api

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,1 +1,4 @@
-export * from '.';
+export { SpeechModule } from './module';
+export { SpeechService } from './speech.service';
+export { SpeechActionDirective } from './speech-action.directive';
+export { SpeechContextDirective } from './speech-context.directive';
